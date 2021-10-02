15ea6f6dc37013308832ccdbf52fd22a52bda5e7
fernando gomes <fernando.gmsoli@gmail.com>
2021-10-02 00:22:08 -0300

    Create util

---
+++ app/src/main/java/com/fgomes/wl_flavors/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.fgomes.wl_flavors.util
+
+const val COLLECTION_ROOT = "data"
+const val COLLECTION_PRODUCTS = "products"
+const val STORAGE_IMAGE = "images"
+const val PRODUCT_KEY = "product"
