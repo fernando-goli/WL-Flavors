d2a5b74eb2fe59c21202e23c5c299e02800daff0
fernando gomes <fernando.gmsoli@gmail.com>
2021-10-03 23:59:52 -0300

    feat: new fragment(add product)

---
+++ app/src/main/java/com/fgomes/wl_flavors/ui/addproduct/AddProductFragment.kt
@@ -0,0 +1,88 @@
+package com.fgomes.wl_flavors.ui.addproduct
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
+import com.fgomes.wl_flavors.databinding.AddProductFragmentBinding
+import com.fgomes.wl_flavors.util.CurrencyTextWatcher
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputLayout
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddProductFragment : BottomSheetDialogFragment() {
+
+    private var _binding: AddProductFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private var imageUri: Uri? = null
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        imageUri = uri
+        binding.ivProduct.setImageURI(uri)
+    }
+
+    private val viewModel: AddProductViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = AddProductFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observerVMEvents()
+        setListeners()
+    }
+
+    private fun observerVMEvents(){
+        viewModel.imageUriErrorResId.observe(viewLifecycleOwner){ drawableResId ->
+            binding.ivProduct.setBackgroundResource(drawableResId)
+        }
+
+        viewModel.descriptionFieldErrorResId.observe(viewLifecycleOwner){ stringResId ->
+            binding.tilDescription.setError(stringResId)
+        }
+
+        viewModel.priceFieldErrorResId.observe(viewLifecycleOwner){ stringResId ->
+            binding.tilPrice.setError(stringResId)
+        }
+
+    }
+
+    private fun TextInputLayout.setError (stringResId: Int?){
+        error = if(stringResId != null){
+            getString(stringResId)
+        } else null
+    }
+
+    private fun setListeners(){
+        binding.ivProduct.setOnClickListener {
+            chooseImage()
+        }
+
+        binding.btAddProduct.setOnClickListener {
+            val description = binding.tieDescription.text.toString()
+            val price = binding.tiePrice.text.toString()
+
+            viewModel.createProduct(description, price, imageUri)
+        }
+
+        binding.tiePrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this) )
+        }
+    }
+
+    private fun chooseImage() {
+        getContent.launch("image/*")
+    }
+
+}
---
+++ app/src/main/java/com/fgomes/wl_flavors/ui/addproduct/AddProductViewModel.kt
@@ -0,0 +1,63 @@
+package com.fgomes.wl_flavors.ui.addproduct
+
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fgomes.wl_flavors.R
+import com.fgomes.wl_flavors.domain.usecase.CreateProductUseCase
+import com.fgomes.wl_flavors.util.fromCurrency
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddProductViewModel @Inject constructor(
+    private val createProductUseCase: CreateProductUseCase
+) : ViewModel() {
+
+    private var isFormValid = false
+
+    private val _imageUriErrorResId = MutableLiveData<Int>()
+    val imageUriErrorResId: LiveData<Int> = _imageUriErrorResId
+
+    private val _descriptionFieldErrorResId = MutableLiveData<Int>()
+    val descriptionFieldErrorResId: LiveData<Int?> = _descriptionFieldErrorResId
+
+    private val _priceFieldErrorResId = MutableLiveData<Int>()
+    val priceFieldErrorResId: LiveData<Int?> = _priceFieldErrorResId
+
+    fun createProduct(description: String, price: String, imageUri: Uri?) = viewModelScope.launch {
+        isFormValid = true
+
+        _imageUriErrorResId.value = getDrawableResIdIfNull(imageUri)
+        _descriptionFieldErrorResId.value = getErrorStringResIdIfEmpty(description)
+        _priceFieldErrorResId.value = getErrorStringResIdIfEmpty(price)
+
+        if(isFormValid){
+            try{
+                val product = createProductUseCase(description, price.fromCurrency(), imageUri!!)
+
+            } catch (e: Exception){
+                Log.d("CreateProduct", e.toString())
+            }
+        }
+
+    }
+
+    private fun getErrorStringResIdIfEmpty(value: String): Int?{
+        return if (value.isEmpty()){
+            isFormValid = false
+            R.string.add_product_field_error
+        } else null
+    }
+
+    private fun getDrawableResIdIfNull(value: Uri?): Int{
+        return if (value == null) {
+            isFormValid = false
+            R.drawable.background_product_image_error
+        } else R.drawable.background_product_image
+    }
+}
