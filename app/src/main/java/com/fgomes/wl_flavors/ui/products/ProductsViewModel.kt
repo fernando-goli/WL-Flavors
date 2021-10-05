608f3245e2c983337c511c1232d812793ad6e226
fernando gomes <fernando.gmsoli@gmail.com>
2021-10-04 21:37:36 -0300

    Created ProductsViewModel

---
+++ app/src/main/java/com/fgomes/wl_flavors/ui/products/ProductsViewModel.kt
@@ -0,0 +1,35 @@
+package com.fgomes.wl_flavors.ui.products
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fgomes.wl_flavors.config.Config
+import com.fgomes.wl_flavors.domain.model.Product
+import com.fgomes.wl_flavors.domain.usecase.GetProductsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductsViewModel @Inject constructor(
+    private val getProductsUseCase: GetProductsUseCase,
+    config: Config
+) : ViewModel() {
+
+    private val _productsData = MutableLiveData<List<Product>>()
+    val productsData: LiveData<List<Product>> = _productsData
+
+    private val _addButtonVisibilityData = MutableLiveData(config.addButtonVisibility)
+    val addButtonVisibilityData : LiveData<Int> = _addButtonVisibilityData
+
+    fun getProducts() = viewModelScope.launch {
+        try {
+            val products = getProductsUseCase()
+            _productsData.value = products
+        }catch (e: Exception){
+            Log.d("ProductsViewModel", e.toString())
+        }
+    }
+}
